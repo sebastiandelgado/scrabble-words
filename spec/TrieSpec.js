551500558d83668cedcfb023185ddb1e5d544891
Sebastian Delgado <sdalcega@gmail.com>
2014-09-16 09:18:48 -0700

    Added tiles cloner

--- spec/TrieSpec.js
+++ spec/TrieSpec.js
@@ -1,22 +1,29 @@
 describe('Insert', function() {
 
   it('should contain a value added to it', function() {
-  	var t = new Trie();
-  	t.insert('sound');
-  	t.insert('sounder');
-  	t.insert('soundest');
-  	t.insert('maul');
-  	expect(t.contains('sound')).to.equal(true);
-  	expect(t.contains('sounder')).to.equal(true);
-  	expect(t.contains('soundest')).to.equal(true);
-  	expect(t.contains('maul')).to.equal(true);
+    var t = new Trie();
+    t.insert('sound');
+    t.insert('sounder');
+    t.insert('soundest');
+    t.insert('maul');
+    expect(t.contains('sound')).to.equal(true);
+    expect(t.contains('sounder')).to.equal(true);
+    expect(t.contains('soundest')).to.equal(true);
+    expect(t.contains('maul')).to.equal(true);
   });
 
   it('should contain two strings if one is a subset of the other', function() {
-  	var t = new Trie();
-  	t.insert('sound');
-  	t.insert('sounder');
-  	expect(t.contains('sound')).to.equal(true);
-  	expect(t.contains('sounder')).to.equal(true);
+    var t = new Trie();
+    t.insert('sound');
+    t.insert('sounder');
+    expect(t.contains('sound')).to.equal(true);
+    expect(t.contains('sounder')).to.equal(true);
   });
+
+  it('should test something im not sure of', function() {
+    var t = new Trie();
+    t.insert("abc");
+    t.insert("ac");
+    expect(t.contains("ab")).to.equal(false);
+  })
 });
--- src/findScrabbleWords.js
+++ src/findScrabbleWords.js
@@ -1,6 +1,6 @@
 var findScrabbleWords = function(trieDictionary) {
 
-	var tilesCounts = {
+	var allTiles = {
 		e:12,
 		a:9, i:9,
 		o:8,
@@ -10,7 +10,9 @@ var findScrabbleWords = function(trieDictionary) {
 		b:2, c:2, m:2, p:2, f:2, h:2, v:2, w:2, y:2,
 		k:1, j:1, x:1, q:1, z:1
 	};
-	var verifiableScrabbleWord = function(word) {
+	var verifyScrabbleWord = function(word) {
+		var wordLetters = {};
+		
 	};
 	var cloneTiles = function(tiles) {
 		var clone = {};
@@ -28,12 +30,21 @@ var findScrabbleWords = function(trieDictionary) {
 	// push to words found
 
 	// will check the current node to see if wordsofar is completed
-	//Will then make a recursive call to every branch that the tiles will let it. 
-	var searchTrie = function(node, wordSoFar) {
-
+	// Will then make a recursive call to every branch that the tiles will let it. 
+	var searchTrie = function(node, wordSoFar, tilesLeft) {
+		if (node.wordEnd) {
+			wordsFound.push(wordSoFar);
+		}
+		for (var l in node.letters) {
+			if(tilesLeft[l] > 0) {
+				var newTiles = cloneTiles(tilesLeft);
+				newTiles[l]--;
+				searchTrie(node.letters[l], wordSoFar+l, newTiles);
+			}
+		}
 	};
 
-	serchTrie(trieDictionary.root, '');
+	searchTrie(trieDictionary.root, '', allTiles);
 
 	return wordsFound;
 
